bf2af246519bd49718046fda4c551b39b3fea3eb
elynn <elynn@vagrant.lan>
2016-12-16 19:47:39 +0800

    Add vsphere-vagrant example

---
+++ vagrant/kube/common.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=http://yum.kubernetes.io/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+sudo yum install -y docker kubelet kubeadm kubectl kubernetes-cni
+sudo systemctl enable docker && systemctl start docker
+sudo systemctl enable kubelet && systemctl start kubelet
---
+++ vagrant/kube/roles/master/files/get_token.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+token=$(kubectl -n kube-system get secret clusterinfo -o yaml | grep token-map | awk '{print $2}' | base64 -d | sed "s|{||g;s|}||g;s|:|.|g;s/\"//g;" | xargs echo)
+echo $token
